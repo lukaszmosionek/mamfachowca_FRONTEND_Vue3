338d68a8b67891f4e96688a7c84d6c295f6bd419
lukaszmosionek <mosioneklukasz@gmail.com>
2025-06-29 11:16:30 +0200

    fix: move routes to externel file

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
-import HomeView from '../views/HomeView.vue'
-import LoginView from '../views/LoginView.vue'
-import RegisterView from '../views/RegisterView.vue'
-import MyServicesView from '../views/MyServicesView.vue'
+import { useAuthStore } from '@/stores/auth'
+import routes from './routes'
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [
-      { path: '/', name: 'Home', component: HomeView },
-      { path: '/login', name: 'Login', component: LoginView },
-      { path: '/register', name: 'Register', component: RegisterView },
-      { path: '/my-services', name: 'MyServices', component: MyServicesView },
-  ],
+  routes,
+})
+
+// Autoryzacja tras
+router.beforeEach(async (to, from, next) => {
+  const auth = useAuthStore()
+
+  const requiresAuth = to.meta.requiresAuth
+  const requiredRole = to.meta.role
+
+  if (requiresAuth && !auth.token) {
+    return next({ name: 'Login' })
+  }
+
+  if (requiresAuth && requiredRole && auth.user?.role !== requiredRole) {
+    return next({ name: 'Home' }) // lub np. 403
+  }
+
+  next()
 })
 
 export default router
