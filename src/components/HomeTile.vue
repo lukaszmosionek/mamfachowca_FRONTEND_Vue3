ac64f735f8632e67ec91ee605c805461d315cc7a
lukaszmosionek <mosioneklukasz@gmail.com>
2025-08-01 15:47:06 +0200

    style: improve formatting and readability in HomeTile component

--- src/components/HomeTile.vue
+++ src/components/HomeTile.vue
@@ -1,44 +1,53 @@
 <template>
-<!-- <!-- <div v-if="!services" class="text-center mt-4">{{ $t('No services') }}</div> -->
-<div v-if="services">
-<div class="mx-auto bg-white rounded shadow-md overflow-hidden flex mt-5" v-for="(s, index) in services" :key="s.id">
+  <!-- <!-- <div v-if="!services" class="text-center mt-4">{{ $t('No services') }}</div> -->
+  <div v-if="services">
+    <div class="mx-auto bg-white rounded shadow-md overflow-hidden flex mt-5" v-for="(s, index) in services"
+      :key="s.id">
 
-    <!-- Zdjęcie -->
-    <div class="relative w-80 h-80">
-      <img :src="s.photos?.[0]?.photo_path ?? noPhoto" alt="{{ s.name }}" class="w-full h-full object-cover">
-      <div class="absolute bottom-2 left-2 bg-teal-500 text-white text-xs font-bold px-2 py-1 rounded">
-        WYRÓŻNIONE
-      </div>
-    </div>
-
-    <!-- Opis -->
-    <div class="flex-1 ">
-    <div class="p-4 flex flex-col justify-between h-full">
-
-      <div>
-        <h2 class="text-lg font-semibold text-gray-800">{{ s.name }}</h2>
-        <p v-if="!isLoading" class="text-sm text-gray-500 hidden md:block">{{ s?.description?.slice(0, 00)+'...' }}</p>
-        <div v-if="isLoading" class="spinner"></div>
-        <div class="text-sm text-gray-600 mt-4" v-if="s?.provider">
-          <!-- <span>🚗</span> -->
-          <div>Service duration: {{ s.duration }} min.</div>
-          <div>Provider: <RouterLink class="" :to="{ name: 'Profile', params: { userId: s?.provider?.id ?? 0 } }">{{ s?.provider?.name }}</RouterLink> <RouterLink class="text-2xl" :to="{ name: 'Messages', params: { userId: s?.provider?.id ?? 0 } }">&#9993;</RouterLink> </div>
-          <BaseButton @click="router.push({ name: 'BookServiceView', params:{serviceId: s.id} })" :name="$t('Book')" class="float-right"/>
+      <!-- Zdjęcie -->
+      <div class="relative w-80 h-80">
+        <img :src="s.photos?.[0]?.photo_path ?? noPhoto" alt="{{ s.name }}" class="w-full h-full object-cover">
+        <div class="absolute bottom-2 left-2 bg-teal-500 text-white text-xs font-bold px-2 py-1 rounded">
+          WYRÓŻNIONE
         </div>
       </div>
 
-      <!-- Cena i serduszko -->
-      <div class="flex justify-between items-center mt-1" v-if="s.price">
-        <span class="text-lg font-bold text-gray-800">{{ n( Number(s.price), 'currency') }}</span>
-        <span @click="toggleFavorite(s.id, $event)" class="text-gray-400 hover:text-red-500 cursor-pointer text-3xl">{{ s.is_favorited ? '♥' : '♡' }}</span>
+      <!-- Opis -->
+      <div class="flex-1 ">
+        <div class="p-4 flex flex-col justify-between h-full">
+
+          <div>
+            <h2 class="text-lg font-semibold text-gray-800">{{ s.name }}</h2>
+            <p v-if="!isLoading" class="text-sm text-gray-500 hidden md:block">{{ s?.description?.slice(0, 200) +
+              '...' }}
+            </p>
+            <div v-if="isLoading" class="spinner"></div>
+            <div class="text-sm text-gray-600 mt-4" v-if="s?.provider">
+              <!-- <span>🚗</span> -->
+              <div>Service duration: {{ s.duration }} min.</div>
+              <div>Provider: <RouterLink class="" :to="{ name: 'Profile', params: { userId: s?.provider?.id ?? 0 } }">{{
+                s?.provider?.name }}</RouterLink>
+                <RouterLink class="text-2xl" :to="{ name: 'Messages', params: { userId: s?.provider?.id ?? 0 } }">
+                  &#9993;</RouterLink>
+              </div>
+              <BaseButton @click="router.push({ name: 'BookServiceView', params: { serviceId: s.id } })"
+                :name="$t('Book')" class="float-right" />
+            </div>
+          </div>
+
+          <!-- Cena i serduszko -->
+          <div class="flex justify-between items-center mt-1" v-if="s.price">
+            <span class="text-lg font-bold text-gray-800">{{ n(Number(s.price), 'currency') }}</span>
+            <span @click="toggleFavorite(s.id, $event)"
+              class="text-gray-400 hover:text-red-500 cursor-pointer text-3xl">{{ s.is_favorited ? '♥' : '♡' }}</span>
+          </div>
+
+        </div>
       </div>
 
     </div>
-    </div>
-
   </div>
-</div>
-<div v-else class="text-center mt-4">{{ $t('No services') }}</div>
+  <div v-else class="text-center mt-4">{{ $t('No services') }}</div>
 
 </template>
 
@@ -69,7 +78,7 @@ const props = defineProps({
 });
 
 const toggleFavorite = async (itemId, event) => {
-if( !authStore.token ) return toast(t('Login to add to favorites'))
+  if (!authStore.token) return toast(t('Login to add to favorites'))
   const el = event.currentTarget;
   event.currentTarget.classList.add('animate-bounce');
 
