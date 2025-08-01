a87593ab7f944f81a08af60d2d9e6a38933e7cc0
lukaszmosionek <mosioneklukasz@gmail.com>
2025-08-01 16:19:28 +0200

    style: enhance layout and improve loading state handling in MessagesView component

--- src/views/MessagesView.vue
+++ src/views/MessagesView.vue
@@ -1,60 +1,61 @@
 <template>
-<div class="py-4 wrapper">
+  <div class="py-4 wrapper">
 
     <h2 class="text-2xl font-semibold mb-5 text-gray-800 text-center">{{ $t('Messages') }}</h2>
 
-    <div class="d-flex" style="display: flex;">
-
+    <div class="flex">
 
       <!-- // Messages List -->
-          <div class="mb-4 w-1/4 pr-4">
-            <h3 class="text-lg font-medium mb-2 text-gray-800">{{ $t('People you have chatted with') }}</h3>
-            <ul class="space-y-2">
-              <li v-for="person in messagedPeople" :key="person.id" class="overflow-hidden text-ellipsis whitespace-nowrap">
-                <RouterLink :to="{ name: 'Messages', params: { userId: person.id ?? 0 } }" class="text-blue-500 hover:underline" :class="user.receiverUser.id === person.id ? 'font-bold' : ''" :title="person.email">
-                  {{ person.name }}
-                </RouterLink>
-              </li>
-            </ul>
-            <span v-if="messagedPeople.length === 0" class="text-gray-500">{{ $t('No messages yet') }}</span>
+      <div class="mb-4 w-1/4 pr-4">
+        <h3 class="text-lg font-medium mb-2 text-gray-800">{{ $t('People you have chatted with') }}</h3>
+        <ul class="space-y-2">
+          <li v-for="person in messagedPeople" :key="person.id" class="overflow-hidden text-ellipsis whitespace-nowrap">
+            <RouterLink :to="{ name: 'Messages', params: { userId: person.id ?? 0 } }"
+              class="text-blue-500 hover:underline" :class="{ 'font-bold': user.receiverUser.id === person.id }"
+              :title="person.email">
+              {{ person.name }}
+            </RouterLink>
+          </li>
+        </ul>
+        <span v-if="messagedPeople.length === 0" class="text-gray-500">{{ $t('No messages yet') }}</span>
+      </div>
+      <!-- // Messages List -->
+
+      <div class="w-3/4 spl-4">
+
+        <div class="border p-4 h-96 overflow-y-scroll scroll-smooth space-y-2 text-gray-700" ref="scrollRef">
+
+          <div v-if="isFetchingMessages" class="spinner"></div>
+          <div v-else>
+            <div class="font-bold text-lg text-center">{{ user.receiverUser.name }}</div>
+            <div v-for="msg in messages" :key="msg.id" class="text-sm">
+              <div v-if="msg?.sender_id === user.currentUser.id" class="text-right">
+                <strong>{{ $t('You') }}:</strong> {{ msg.body }}
+              </div>
+
+              <div v-if="msg?.sender_id !== user.currentUser.id" class="text-left">
+                <strong>{{ user.receiverUser.name }}:</strong> {{ msg.body }}
+              </div>
+            </div>
           </div>
-
-
-          <div class="w-3/4 spl-4">
-
-              <div class="border p-4 h-96 overflow-y-scroll scroll-smooth space-y-2 text-gray-700" ref="scrollRef">
-
-                <div class="font-bold text-lg text-center">{{ user.receiverUser.name }}</div>
-                <div v-for="msg in messages" :key="msg.id" class="text-sm">
-                    <div v-if="msg?.sender_id === user.currentUser.id" class="text-right">
-                      <strong>{{ $t('You') }}:</strong> {{ msg.body }}
-                    </div>
-
-                    <div v-if="msg?.sender_id !== user.currentUser.id" class="text-left">
-                      <strong>{{ user.receiverUser.name }}:</strong> {{ msg.body }}
-                    </div>
-                </div>
-                <!-- <div v-if="messages.length === 0" class="text-center text-gray-500">
+          <!-- <div v-if="messages.length === 0" class="text-center text-gray-500">
                   {{ $t('No messages yet') }}
                 </div> -->
 
-                <div v-if="loading" class="flex items-center justify-center bg-gray-100">
-                  <div class="flex flex-col items-center space-y-4">
-                    <div class="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
-                    <p class="text-gray-700 text-lg font-medium">{{ $t('Loading...') }}</p>
-                  </div>
-                </div>
 
-              </div>
-              <form @submit.prevent="sendMessage" class="mt-4 flex">
-                <BaseInput v-model="newMessage" type="text" max="1000" :placeholder="$t('Message')" :errors="errors.message" class="flex-1 border rounded p-2 text-gray-700" />
-                <BaseButton :name="$t('Send')" :loading="loading" class="ml-2 bg-blue-500 text-white px-4 py-2 rounded"/>
-              </form>
+
         </div>
+        <form @submit.prevent="sendMessage" class="mt-4 flex gap-4">
+          <BaseInput v-model="newMessage" type="text" max="1000" :placeholder="$t('Message')" :errors="errors.message"
+            class="w-full" />
+          <BaseButton :name="$t('Send')" :loading="isSendingMessage" :disabled="isFetchingMessages || isSendingMessage"
+            class="px-5 h-12" />
+        </form>
+      </div>
     </div>
 
 
-</div>
+  </div>
 </template>
 
 <script setup>
@@ -65,7 +66,6 @@ import { useRoute } from 'vue-router'
 import BaseButton from '@/components/BaseButton.vue'
 import BaseInput from '@/components/BaseInput.vue'
 import { toast } from 'vue3-toastify'
-// const props = defineProps({ receiverId, currentUser })
 import { useI18n } from 'vue-i18n'
 import notificationSound from '@/assets/notification.wav'
 
@@ -75,7 +75,8 @@ const { t } = useI18n()
 const messages = ref([])
 const newMessage = ref('')
 const route = useRoute()
-const loading = ref(false)
+const isSendingMessage = ref(false)
+const isFetchingMessages = ref(false)
 const errors = ref({})
 const scrollRef = ref(null)
 const messagedPeople = ref([])
@@ -86,18 +87,18 @@ const user = ref({
 })
 
 const fetchMessages = async () => {
-  loading.value = true
-  try{
-    const res = await api.get('/users/'+route.params.userId+'/messages');
+  isFetchingMessages.value = true
+  try {
+    const res = await api.get('/users/' + route.params.userId + '/messages');
     messages.value = res.data.messages
     user.value.receiverUser = res.data.receiver
     scrollToBottom()
   } catch (error) {
     if (error.response && error.response.status === 404) {
-        alert('User not found (404)');
-      }
+      alert('User not found (404)');
     }
-  loading.value = false
+  }
+  isFetchingMessages.value = false
 }
 
 const fetchMessagedPeople = async () => {
@@ -108,14 +109,14 @@ const fetchMessagedPeople = async () => {
 
 const sendMessage = async () => {
   errors.value = {} // reset errors before submit
-  loading.value = true
+  isSendingMessage.value = true
   try {
-    const res = await api.post('/users/'+user.value.receiverUser.id+'/messages', {
+    const res = await api.post('/users/' + user.value.receiverUser.id + '/messages', {
       message: newMessage.value
     })
     newMessage.value = ''
     messages.value.push(res.data)
-    if(!messagedPeople.value.find(p => p.id === user.value.receiverUser.id)) {
+    if (!messagedPeople.value.find(p => p.id === user.value.receiverUser.id)) {
       messagedPeople.value.unshift(user.value.receiverUser) //add an element to the beginning/end of an array
     }
     scrollToBottom()
@@ -123,7 +124,7 @@ const sendMessage = async () => {
   } catch (error) {
     errors.value = error;
   }
-  loading.value = false
+  isSendingMessage.value = false
 }
 
 watch(route, (newRoute) => {
@@ -135,7 +136,7 @@ watch(route, (newRoute) => {
 onMounted(() => {
   user.value.currentUser = JSON.parse(localStorage.getItem('user'))
 
-  if(route.params.id == user.value.currentUser.id) {
+  if (route.params.id == user.value.currentUser.id) {
     alert('You are chatting with yourself')
   }
 
@@ -146,7 +147,7 @@ onMounted(() => {
     .listen('MessageSent', (e) => {
       if (e.message.sender_id === user.value.receiverUser.id) {
         messages.value.push(e.message)
-        toast.success( t('New message received:') + ' ' + e.message.body.slice(0, 20) + '...')
+        toast.success(t('New message received:') + ' ' + e.message.body.slice(0, 20) + '...')
         scrollToBottom()
         playSound()
       }
