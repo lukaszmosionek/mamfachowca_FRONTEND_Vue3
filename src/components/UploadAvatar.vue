da09b52882cfa016ea8f957ea91b683f45a9ae65
lukaszmosionek <mosioneklukasz@gmail.com>
2025-08-24 17:00:39 +0200

    fix(UploadAvatar, i18n): update avatar upload success message and add localization keys

--- src/components/UploadAvatar.vue
+++ src/components/UploadAvatar.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="flex flex-col items-center">
         <div class="relative">
-            <img :src="user.avatar || defaultAvatar" alt="User Avatar" class="rounded-full w-60 h-60" loading="lazy" :class="{ 'opacity-50':  isLoading}" />
+            <img :src="user.avatar || defaultAvatar" alt="User Avatar" class="rounded-full w-60 h-60 object-cover" loading="lazy" :class="{ 'opacity-50':  isLoading}" />
             <div class="absolute left-1/2 top-1/2 -ml-[20px] -mt-[20px] z-50" v-if="isLoading">
                 <div class="spinner"></div>
             </div>
           </div>
 
           <label for="fileUpload" class="mt-2 flex items-center justify-center px-4 py-2 bg-blue-600 text-white rounded-lg shadow-md cursor-pointer hover:bg-blue-700 transition duration-200">
-              <font-awesome-icon :icon="['fas', 'image']" /> {{ $t('Change avatar') }}
+              <font-awesome-icon :icon="['fas', 'image']" /> {{ $t('avatar.change') }}
               <input type="file" @change="onFileChange" accept="image/*" id="fileUpload" class="hidden" />
           </label>
 
@@ -20,8 +20,8 @@ import api from '@/services/api'
 import Swal from 'sweetalert2'
 import { ref } from 'vue'
 import { toast } from 'vue3-toastify'
-import BaseButton from './BaseButton.vue'
 import defaultAvatar from '@/assets/default-avatar.svg';
+import { useI18n } from 'vue-i18n'
 
 defineProps({
   user: {
@@ -30,6 +30,7 @@ defineProps({
   }
 })
 
+const { locale, t } = useI18n()
 const emit = defineEmits(['avatar-changed'])
 const file = ref(null)
 const isLoading = ref(false)
@@ -50,12 +51,10 @@ async function uploadAvatar() {
         'Content-Type': 'multipart/form-data'
       }
     })
-    toast.success('Avatar uploaded!')
+    toast.success( t('avatar.uploaded-successfully'))
     emit('avatar-changed', response.avatar_url) // 🔥 Emit the new avatar URL
-    console.log('avatar_url'+response.avatar_url)
   } catch (err) {
     Swal.fire(err.response.data.message, 'error', 'error')
-    console.error(err)
   }
   isLoading.value = false
 }
